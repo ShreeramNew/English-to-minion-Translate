5439d82a71df9d913fd6da4786fdb6653199401f
ShreeramNew <shreerambca1@gmail.com>
2023-03-19 21:19:21 +0530

    fix-fixed extra padding

--- scriptjs.js
+++ scriptjs.js
@@ -2,7 +2,6 @@
 var button_click = document.querySelector(".button");
 var input_area = document.querySelector(".input_text");
 var output_Text = document.querySelector(".output_text");
-// var out_title = document.querySelector(".out-title");
 var visible = "visible";
 
 
@@ -14,13 +13,11 @@ button_click.addEventListener("click", () => {
         return;
     }
     output_Text.innerHTML="";
-   
     var input_text = input_area.value;
     var final_url = "https://api.funtranslations.com/translate/minion.json"+ "?text=" + input_text;
     fetch(final_url)
     .then(promise=>promise.json())
     .then(jsonResult=>{
-        // out_title.style.visibility=visible;
         output_Text.style.visibility=visible;
         output_Text.innerText=jsonResult.contents.translated;
     })
